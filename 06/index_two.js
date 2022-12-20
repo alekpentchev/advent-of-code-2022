ad7b38874381791e413fea68fd760ccd2c293bef
Aleksander Pentchev <alek.pentchev@gmail.com>
2022-12-20 14:24:04 +0100

    day 6 - part two

---
+++ 06/index_two.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync("input.txt", "utf-8");
+const characters = input.split("");
+
+const marker = new Set();
+
+for (let index = 0; index < characters.length; index++) {
+  const character = characters[index];
+  if (marker.has(character)) {
+    marker.clear();
+    continue;
+  }
+
+  marker.add(character);
+
+  if (marker.size === 14) {
+    console.log("the result is: ", marker, index + 1);
+    break;
+  }
+}
